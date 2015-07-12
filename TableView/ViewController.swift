8e9b4a38e236027044bcf14456fe7c0308a5249b
Rex Liu <rex@RexdeMacBook-Pro.local>
2015-07-12 17:07:47 +0800

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Rex Liu on 2015/7/12.
+//  Copyright (c) 2015年 Rex Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
